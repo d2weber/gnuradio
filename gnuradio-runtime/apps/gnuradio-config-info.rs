8e74915eb701231aef3c80cc08a8257cf13026ca
d2weber <29163905+d2weber@users.noreply.github.com>
2025-01-26 21:53:20 +0100

    Add cxx->rust

--- gnuradio-runtime/apps/gnuradio-config-info.rs
+++ gnuradio-runtime/apps/gnuradio-config-info.rs
@@ -1,10 +1,18 @@
-#[cxx::bridge(namespace = "grust")]
+use ffi::my_magic_number;
+
+#[cxx::bridge]
 mod ffi {
+    unsafe extern "C++" {
+        include!("gnuradio/my_function.h");
+        fn my_magic_number() -> u32;
+    }
+
+    #[namespace = "grust"]
     extern "Rust" {
         fn hello() -> u32;
     }
 }
 
 fn hello() -> u32 {
-    1337
+    my_magic_number()
 }
---
+++ gnuradio-runtime/include/gnuradio/my_function.h
@@ -0,0 +1,6 @@
+#include <cstdint>
+
+inline std::uint32_t my_magic_number()
+{
+  return 42;
+}
